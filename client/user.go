84e7b70e503364f40b19f5796c7dbf6ce6368639
Tom Siouan <tomsiouan@icloud.com>
2025-09-19 12:31:18 +0200

    Update(client) user.go to include role assignment (include test)

--- client/user.go
+++ client/user.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"path"
 )
 
 func (c *Client) UserGet(ctx context.Context, id string) (*UserModel, error) {
@@ -83,4 +85,74 @@ func (c *Client) UserUpdate(ctx context.Context, user *UserModel) (*UserModel, e
 		return nil, err
 	}
 	return &returnUser, nil
+}
+
+func (c *Client) GetRolesForUser(ctx context.Context, userId string) ([]RoleModel, error) {
+	if userId == "" {
+		return nil, errEmptyID
+	}
+
+	req := &request{
+		method: http.MethodGet,
+		path:   path.Join("api/users", userId, "roles"),
+	}
+
+	res, err := expect(200)(c.do(ctx, req))
+	if err != nil {
+		return nil, err
+	}
+
+	var returnRoles *[]RoleModel
+	if err := decode(res.Body, &returnRoles); err != nil {
+		return nil, err
+	}
+	return *returnRoles, nil
+}
+
+func (c *Client) AssignRolesForUser(ctx context.Context, roleIds *RoleIdsModel, userId string) error {
+	if userId == "" || len(roleIds.RoleIds) <= 0 {
+		return errEmptyID
+	}
+
+	fmt.Println("AssignRolesForUser")
+	fmt.Print(roleIds)
+	fmt.Print(*roleIds)
+
+	req := &request{
+		method: http.MethodPost,
+		path:   path.Join("api/users", userId, "roles"),
+		body:   *roleIds,
+	}
+
+	_, err := expect(201)(c.do(ctx, req))
+	return err
+}
+
+func (c *Client) UpdateRolesForUser(ctx context.Context, roleIds *RoleIdsModel, userId string) error {
+	if userId == "" || len(roleIds.RoleIds) <= 0 {
+		return errEmptyID
+	}
+
+	req := &request{
+		method: http.MethodPut,
+		path:   path.Join("api/users", userId, "roles"),
+		body:   roleIds,
+	}
+
+	_, err := expect(200)(c.do(ctx, req))
+	return err
+}
+
+func (c *Client) DeleteRolesForUser(ctx context.Context, roleId string, userId string) error {
+	if userId == "" || roleId == "" {
+		return errEmptyID
+	}
+
+	req := &request{
+		method: http.MethodDelete,
+		path:   path.Join("api/users", userId, "roles", roleId),
+	}
+
+	_, err := expect(204)(c.do(ctx, req))
+	return err
 }
